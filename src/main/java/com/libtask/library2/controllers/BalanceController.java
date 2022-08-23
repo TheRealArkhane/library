65bbbfeca1efdefc13b27ed33fa48f9c585a85dd
TheRealArkhane <96845387+TheRealArkhane@users.noreply.github.com>
2022-08-24 01:18:36 +0300

    Update BalanceController.java

--- src/main/java/com/libtask/library2/controllers/BalanceController.java
+++ src/main/java/com/libtask/library2/controllers/BalanceController.java
@@ -1,6 +1,6 @@
 package com.libtask.library2.controllers;
 
-import com.libtask.library2.entities.User;
+import com.libtask.library2.entities.Book;
 import com.libtask.library2.services.BalanceService;
 import com.libtask.library2.services.BookService;
 import com.libtask.library2.services.UserService;
@@ -20,17 +20,18 @@ public class BalanceController {
     private final BookService bookService;
 
     @PutMapping("/take")
-    public User addBookToUserBalance(@RequestParam Long userId, @RequestParam Long bookId) {
+    public Book addBookToUserBalance(@RequestParam Long userId, @RequestParam Long bookId) {
         balanceService.addBookToUserBalance(
                 userService.getUserById(userId),
                 bookService.getBookById(bookId));
-        return userService.getUserById(userId);
+        return bookService.getBookById(bookId);
     }
 
     @PutMapping("/return")
-    public void removeBookFromUserBalance(@RequestParam Long userId, @RequestParam Long bookId) {
+    public Book removeBookFromUserBalance(@RequestParam Long userId, @RequestParam Long bookId) {
         balanceService.removeBookFromUserBalance(
                 userService.getUserById(userId),
                 bookService.getBookById(bookId));
+        return bookService.getBookById(bookId);
     }
 }
