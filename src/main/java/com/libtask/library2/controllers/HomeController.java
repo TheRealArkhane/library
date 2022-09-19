01ffadb78417843d99ed6c48e2fd81bcc222df11
TheRealArkhane <96845387+TheRealArkhane@users.noreply.github.com>
2022-09-19 14:43:07 +0300

    Delete HomeController.java

--- src/main/java/com/libtask/library2/controllers/HomeController.java
+++
@@ -1,9 +0,0 @@
-package com.libtask.library2.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-    //не имею ни малейшего представления, что на данный момент делать с этим контроллером,
-    //когда он REST и MvcConfig уже перенаправляет на HTML темплейт
-}
