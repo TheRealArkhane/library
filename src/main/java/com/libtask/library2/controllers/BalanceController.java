4b7cf7bea4dbe5a1736dfa0e48b8889bca689488
TheRealArkhane <96845387+TheRealArkhane@users.noreply.github.com>
2022-11-19 00:24:09 +0300

    Add files via upload

--- src/main/java/com/libtask/library2/controllers/BalanceController.java
+++ src/main/java/com/libtask/library2/controllers/BalanceController.java
@@ -1,6 +1,7 @@
 package com.libtask.library2.controllers;
 
 import com.libtask.library2.entities.Book;
+import com.libtask.library2.entities.User;
 import com.libtask.library2.repositories.BookRepository;
 import com.libtask.library2.services.BalanceService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,8 +38,9 @@ public class BalanceController {
                 .orElseThrow(() -> new IllegalArgumentException("Book with this ID is not exist"));
     }
 
-    @GetMapping("/user")
-    public Page<Book> showUserBalance(Long userId, @PageableDefault(sort = "id") Pageable page) {
+    @GetMapping("/user/{userId}")
+    public Page<Book> showUserBalance(
+            @PathVariable(value = "userId") Long userId, @PageableDefault(sort = "id") Pageable page, Principal user) {
         return bookRepository.findTakenBooksByUserId(userId, page);
     }
 }
